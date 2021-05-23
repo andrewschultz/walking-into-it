ab004244cd84d42ddf6ee6ed1c73de123c6e44c8
Andrew <blurglecruncheon@gmail.com>
2021-05-23 18:32:57 -0500

    mt.py now fixes #9

---
+++ mt.py
@@ -0,0 +1,28 @@
+# this is a list of functions from my own mytools, which I didn't want to include in the file proper, because I want as few dependencies as possible
+
+import re
+
+def list_nums(my_list, separator=', '):
+    return separator.join([str(x) for x in my_list])
+
+DASH_TO_UNDERSCORE = 1
+KEEP_DASH_UNDERSCORE = 0
+UNDERSCORE_TO_DASH = -1
+
+def cfg_data_split(x, delimiter=":=", to_tuple = True, strip_line = True, dash_to_underscore = KEEP_DASH_UNDERSCORE):
+    if strip_line:
+        x = x.strip()
+    if dash_to_underscore == DASH_TO_UNDERSCORE:
+        x = x.replace("-", "_")
+    if dash_to_underscore == UNDERSCORE_TO_DASH:
+        x = x.replace("_", "-")
+    ary = re.split("[{}]".format(delimiter), x, 1)
+    if to_tuple:
+        return(ary[0], ary[1])
+    return ary # (prefix, data) = general usage in programs
+
+def nohy(x, also_lower = True): # mostly for command line argument usage, so -s is -S is s is S.
+    if x[0] == '-': x = x[1:]
+    if also_lower:
+        return x.lower()
+    return x
