ed921fd9c03e62812c1b6f770d30cd02983a6310
Andrew <blurglecruncheon@gmail.com>
2021-11-17 08:45:18 -0600

    wiaux.py more ROT13 translation

--- wiaux.py
+++ wiaux.py
@@ -3,12 +3,27 @@
 # walking into it auxiliary file
 #
 
-square = [ ' ', 'X', 'O' ]
+import re
+
+square = [ '?', ' ', 'X', 'O' ]
+
+def is_board_line(my_string):
+    return re.search("^[OX\| ]{5,}$", my_string)
 
 def rot13_string_convert(my_string):
     footer_chunk = "-+-+- "
     mss = my_string.rstrip()
     out_string = ''
+    if is_board_line(my_string):
+        paren_val = 0
+        while len(mss) >= 7:
+            paren_val <<= 6
+            paren_val += square.index(mss[1]) + square.index(mss[1]) << 2 + square.index(mss[1]) << 4
+            mss = mss[7:]
+        out_string = "{{}}".format(paren_val)
+        if '\n' in my_string:
+            out_string += "\n"
+        return out_string
     if mss.startswith("{") and mss.endswith("}"):
         this_idx = int(mss[1:-1])
         add_footer = (this_idx > 0)
@@ -16,7 +31,7 @@ def rot13_string_convert(my_string):
         footer_length = 0
         while this_idx > 0:
             footer_length += 1
-            next_fragment = "{}|{}|{} ".format(square[this_idx & 3 - 1], square[(this_idx >> 2) & 3 - 1], square[(this_idx >> 4) & 3 - 1])
+            next_fragment = "{}|{}|{} ".format(square[this_idx & 3], square[(this_idx >> 2) & 3], square[(this_idx >> 4) & 3])
             this_idx >>= 6
             out_string += next_fragment
         out_string = out_string.rstrip()
@@ -25,8 +40,14 @@ def rot13_string_convert(my_string):
         if '\n' in my_string:
             out_string += "\n"
         return out_string
-    return my_string.translate(str.maketrans("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
+    try:
+        return my_string.translate(str.maketrans("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
         "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"))
+    except:
+        import string
+        rot13 = string.maketrans("ABCDEFGHIJKLMabcdefghijklmNOPQRSTUVWXYZnopqrstuvwxyz", 
+        "NOPQRSTUVWXYZnopqrstuvwxyzABCDEFGHIJKLMabcdefghijklm")
+        return string.translate(my_string, rot13)
 
 # testing
 #print(rot13_string_convert("{55}"))
@@ -40,5 +61,16 @@ def rot13_file_convert(file_name):
     fout = open(out_file, "w")
     with open(file_name) as file:
         for (line_count, line) in enumerate (file, 1):
+            if look_ahead:
+                if line.startswith("-+"):
+                    reserve_write = reserve_write[0] + '-' + reserve_write[1:]
+                    fout.write(reserve_write)
+                    continue
+                else:
+                    fout.write(reserve_write)
+            if is_board_line(line):
+                look_ahead = True
+                reserve_write = rot13_string_convert(line)
+                continue
             fout.write(rot13_string_convert(line))
     fout.close()
